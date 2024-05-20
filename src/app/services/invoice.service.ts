71d26e5deb10cf8d07e158b318641d5bac3d6959
Ignacio <104097123+ignaciogiacotto@users.noreply.github.com>
2024-05-20 10:55:25 -0300

    Agregando nuevo componente Form Item

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Invoice } from '../models/invoice';
 import { invoiceData } from '../data/invoice.data';
+import { Item } from '../models/item';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,11 @@ export class InvoiceService {
     return this.invoice.items.reduce((total, item) => total + (item.price * item.quantity), 0);
   }
 
+  save(item: Item): Invoice{
+    this.invoice.items = [... this.invoice.items, item]
+    const total = this.calculateTotal();
+    return {... this.invoice, total};
+  }
   remove(id: number): Invoice {
     this.invoice.items = this.invoice.items.filter(item => item.id != id);
     const total = this.calculateTotal();
