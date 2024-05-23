91c3e32bc1746c3175f0ce4bc88911277f722a5b
Ignacio <104097123+ignaciogiacotto@users.noreply.github.com>
2024-05-23 18:18:02 -0300

    Agregando validacion al Form Item

--- src/app/components/form-item/form-item.component.ts
+++ src/app/components/form-item/form-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Item } from '../../models/item';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'form-item',
@@ -20,14 +20,19 @@ export class FormItemComponent {
     quantity: ''
     }
 
-  onSubmit(): void{
-    this.addItemEventEmitter.emit({id: this.counterId, ...this.item})
-    this.counterId++;
-    this.item = {
-      product: '',
-      price: '',
-      quantity: ''
+  onSubmit(itemForm: NgForm): void{
+    if(itemForm.valid){
+      this.addItemEventEmitter.emit({id: this.counterId, ...this.item})
+      this.counterId++;
+      this.item = {
+        product: '',
+        price: '',
+        quantity: ''
       }
+    };
+    itemForm.reset();
+    itemForm.resetForm();
+    
   }
 
 }
